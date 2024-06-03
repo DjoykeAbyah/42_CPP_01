4f51ccd064a5ec3f588409ec3b0f821ef7002876
DjoykeAbyah <crudendjoyke@gmail.com>
2024-06-03 11:36:45 +0200

    start 01

--- ex00/Zombie.cpp
+++ ex00/Zombie.cpp
@@ -6,7 +6,19 @@
 /*   By: djoyke <djoyke@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/05/28 22:48:09 by djoyke        #+#    #+#                 */
-/*   Updated: 2024/05/28 22:48:10 by djoyke        ########   odam.nl         */
+/*   Updated: 2024/06/03 11:33:58 by dreijans      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include "Zombie.hpp"
+
+Zombie::Zombie(void)
+{
+	std::cout << Zombie::name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+Zombie::~Zombie(void)
+{
+	std::cout << Zombie::name << ": destroyed" << std::endl;
+}
--- ex00/Zombie.hpp
+++ ex00/Zombie.hpp
@@ -6,7 +6,21 @@
 /*   By: djoyke <djoyke@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/05/28 22:48:15 by djoyke        #+#    #+#                 */
-/*   Updated: 2024/05/28 22:48:16 by djoyke        ########   odam.nl         */
+/*   Updated: 2024/06/03 11:34:11 by dreijans      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ZOMBIE_H
+# define ZOMBIE_H
+
+class Zombie
+{
+	private:
+		std::string name;
+	
+	public:
+		Zombie(void);
+		~Zombie(void);
+};
+
+#endif
