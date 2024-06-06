741a301153d043551b4e624a0983737ece14f925
DjoykeReijans <crudendjoyke@gmail.com>
2024-06-06 21:31:27 +0200

    need to clean this up

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dreijans <dreijans@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/06/04 18:16:12 by dreijans      #+#    #+#                 */
-/*   Updated: 2024/06/05 16:20:52 by dreijans      ########   odam.nl         */
+/*   Updated: 2024/06/06 21:29:34 by djoyke        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,38 +31,43 @@ int main()
 	std::string replaceWord;
 
 	std::cout << "Enter filename, word, and word to replace with" << std::endl;
-
 	std::cin >> filename >> word >> replaceWord;
 
-	std::cout << "Filename = " << filename << std::endl;
-	std::cout << "word = " << word << std::endl;
-	std::cout << "word to replace with = " << replaceWord << std::endl;
+	if (word == replaceWord)
+	{
+		std::cerr << "a bit useless don't you think?" << std::endl;
+		return 1;
+	}
 
 	std::ifstream inFile(filename);
 	if (!inFile)
 	{
-		std::cerr << "cant open this file" << filename << std::endl;
+		std::cerr << "cant open this infile" << filename << std::endl;
 		return 1;
 	}
-	std::cout << "file opened sucessfully" << std::endl;
+	std::cout << "infile opened sucessfully" << std::endl;
 	
-	std::ofstream outFile("output.txt"); //add blabla.replace
+	std::ofstream outFile("output.txt");
 	if (!outFile)
 	{
-		std::cerr << "unable to open outfile" << std::endl; //ad blabla.replace
+		std::cerr << "unable to open outfile" << std::endl;
 		return 1;
 	}
+	std::cout << "outfile opened sucessfully" << std::endl;
 	
 	//read from the infile put in outfile
-	std::string input;
-	while (std::getline(inFile, input))
+	std::string readLine;
+	while (std::getline(inFile, readLine))
 	{
-		size_t position = input.find(word);
-		if (position != std::string::npos)
-			std::cout << "Found the word" << std::endl; // not finding all the ocurrences
-		outFile << input << std::endl;
+		size_t position = readLine.find(word);
+		while (position != std::string::npos)
+		{
+			readLine = readLine.substr(0, position) + replaceWord + readLine.substr(position + word.length());
+			position++;
+			position = readLine.find(word, position + replaceWord.length());
+		}
+		outFile << readLine << std::endl;
 	}
-	
 	inFile.close();
 	outFile.close();
 	return 0;
