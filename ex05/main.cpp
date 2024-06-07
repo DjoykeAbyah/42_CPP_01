3d1fae05fc8c1576b775c25b82f6b6c39776f16d
DjoykeReijans <crudendjoyke@gmail.com>
2024-06-07 22:48:32 +0200

    05 done?

--- ex05/main.cpp
+++ ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: djoyke <djoyke@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/06/06 23:02:43 by djoyke        #+#    #+#                 */
-/*   Updated: 2024/06/07 22:46:25 by djoyke        ########   odam.nl         */
+/*   Updated: 2024/06/07 22:47:54 by djoyke        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,9 @@ int main()
     harl.complain("INFO");
     harl.complain("WARNING");
     harl.complain("ERROR");
+
+    harl.complain("");
+    harl.complain("1");
 
     return 0;
 }
